b27a8fd098060f2e990c393018c624a8ab8cc923
tathithuyvn89 <61693986+tathithuyvn89@users.noreply.github.com>
2022-04-03 15:01:37 +0700

    Update hw2_install_ngnix.sh

--- hw2_install_ngnix.sh
+++ hw2_install_ngnix.sh
@@ -9,3 +9,20 @@ wget https://vnexpress.net -O /var/www/html/index.html
 
 # Restart nginx
 sudo systemctl restart nginx
+
+
+
+Bai khac de thuc hanh tao buoi 4
+
+#!/bin/bash
+
+##Install NGINX
+sudo apt-get update && sudo apt install nginx -y
+
+##Download a webpage
+wget -r http://learnyouahaskell.com/
+
+##Copy downloaded page to NGINX root folder
+
+cd learnyouahaskell.com/
+sudo cp *.* /var/www/html
